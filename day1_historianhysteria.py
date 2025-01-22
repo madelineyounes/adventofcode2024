812dae85389f65a6403ad8fbf0792d1b4afde379
Madeline Younes <Madeline.Younes@quantium.com.au>
2025-01-22 15:47:29 +1100

    feat: added day1

---
+++ day1_historianhysteria.py
@@ -0,0 +1,35 @@
+# Day 1: Historian Hysteria
+from numpy import isin
+from collections import Counter
+
+left = []
+right = []
+right_counter = Counter()
+
+with open('day1input.txt', 'r') as file:
+    # Read each line in the file
+    for line in file:
+      distances = line.strip().split('   ')
+      left.append(int(distances[0]))
+      right.append(int(distances[1]))
+      right_counter[int(distances[1])] += 1
+
+sorted_left = sorted(left)
+sorted_right = sorted(right)
+
+freq_dict1 =  {}
+freq_dict2 = {}
+
+total = 0
+similarity = 0
+
+for i in range (0, len(sorted_left)):
+   l = sorted_left[i]
+   r = sorted_right[i]
+
+   diff = l - r
+   total += abs(diff)
+   similarity += l * right_counter.get(l,0)
+
+print (f"total: {total}")
+print (f"similarity: {similarity}")
